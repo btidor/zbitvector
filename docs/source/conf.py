38c3951eb65c15e6c38349d9290fae6da47796c4
Benjamin Tidor <benjamin@tidor.net>
2023-05-08 20:42:03 -0700

    Start on documentation

---
+++ docs/source/conf.py
@@ -0,0 +1,26 @@
+"""
+Configuration file for the Sphinx documentation builder.
+
+For the full list of built-in configuration values, see the documentation:
+https://www.sphinx-doc.org/en/master/usage/configuration.html
+"""
+
+project = "zbitvector"
+release = "1.0b1"
+
+
+extensions = []
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+
+html_theme = "alabaster"
+html_static_path = ["_static"]
+
+html_sidebars = {"**": ["sidebar.html", "localtoc.html", "relations.html"]}
+html_theme_options = {
+    "show_powered_by": False,
+}
+html_show_copyright = False
+html_show_sourcelink = False
