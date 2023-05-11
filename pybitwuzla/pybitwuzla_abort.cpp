eb4b14e1f582e36ca559d3fcaf08d1053a2b61cc
Benjamin Tidor <benjamin@tidor.net>
2023-05-10 23:36:52 -0700

    Copy pybitwuzla sources into tree for Cython build

---
+++ pybitwuzla/pybitwuzla_abort.cpp
@@ -0,0 +1,44 @@
+// Generated from https://github.com/bitwuzla/bitwuzla at 1230d80
+
+/***
+ * Bitwuzla: Satisfiability Modulo Theories (SMT) solver.
+ *
+ * This file is part of Bitwuzla.
+ *
+ * Copyright (C) 2007-2022 by the authors listed in the AUTHORS file.
+ *
+ * See COPYING for more information on using this software.
+ */
+
+#include "pybitwuzla_abort.h"
+
+#include <string>
+
+class BitwuzlaException : public std::exception
+{
+ public:
+  BitwuzlaException(const char* msg) : msg(msg) {}
+  const char* what() const noexcept override { return msg.c_str(); }
+
+ protected:
+  std::string msg;
+};
+
+void
+pybitwuzla_abort_fun(const char* msg)
+{
+  throw BitwuzlaException(msg);
+}
+
+const char*
+pybitwuzla_get_err_msg()
+{
+  try
+  {
+    throw;
+  }
+  catch (const BitwuzlaException& e)
+  {
+    return e.what();
+  }
+}
