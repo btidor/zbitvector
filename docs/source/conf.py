e1e8f922c955addade831b79f8eca99f3e6870dd
Benjamin Tidor <benjamin@tidor.net>
2023-05-14 16:36:53 -0700

    Docs: set up custom stylesheet, resize headings

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,6 +17,7 @@ exclude_patterns = []
 
 html_theme = "alabaster"
 html_static_path = ["_static"]
+html_css_files = ["custom.css"]
 
 html_sidebars = {"**": ["sidebar.html", "localtoc.html", "relations.html"]}
 html_theme_options = {
