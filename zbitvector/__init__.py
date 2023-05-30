0e67519e4cab75176903155a41e84093ae337fcd
Benjamin Tidor <benjamin@tidor.net>
2023-05-30 16:49:32 -0700

    Use setuptools-scm to detect version number

--- zbitvector/__init__.py
+++ zbitvector/__init__.py
@@ -1,3 +1,12 @@
+"""zbitvector: an efficient, well-typed interface to the Z3 and Bitwuzla SMT solvers."""
+
 from .bitvector import Int as Int
 from .bitvector import Uint as Uint
 from .constraint import Constraint as Constraint
+
+from importlib import metadata
+
+try:
+    __version__ = metadata.version(__name__)
+except metadata.PackageNotFoundError:
+    __version__ = "dev"
