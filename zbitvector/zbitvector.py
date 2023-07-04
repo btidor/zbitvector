412697d4b23aa8c23d9ca254ec89f5858baef975
Benjamin Tidor <benjamin@tidor.net>
2023-07-03 21:38:58 -0700

    Factor out common operations, adjust signatures

--- zbitvector/zbitvector.py
+++ zbitvector/zbitvector.py
@@ -12,7 +12,7 @@ class Symbolic(abc.ABC):
     _term: Final[BitwuzlaTerm]
 
     @abc.abstractmethod
-    def __init__(self, term: BitwuzlaTerm) -> None:
+    def __init__(self, term: BitwuzlaTerm, /) -> None:
         self._term = term
 
     @classmethod
@@ -34,12 +34,38 @@ class Symbolic(abc.ABC):
     def smtlib(self) -> str:
         return self._term.dump("smt2")
 
+    def __eq__(  # pyright: ignore[reportIncompatibleMethodOverride]
+        self, other: Self, /
+    ) -> Constraint:
+        """
+        >>> Uint8(1) == Uint8(3)
+        Constraint(`false`)
+        """
+        return Constraint.from_expr(Kind.EQUAL, self._term, other._term)
+
+    def __ne__(  # pyright: ignore[reportIncompatibleMethodOverride]
+        self, other: Self, /
+    ) -> Constraint:
+        return Constraint.from_expr(Kind.DISTINCT, self._term, other._term)
+
+    def __invert__(self) -> Self:
+        return self.from_expr(Kind.BV_NOT, self._term)
+
+    def __and__(self, other: Self, /) -> Self:
+        return self.from_expr(Kind.BV_AND, self._term, other._term)
+
+    def __or__(self, other: Self, /) -> Self:
+        return self.from_expr(Kind.BV_OR, self._term, other._term)
+
+    def __xor__(self, other: Self, /) -> Self:
+        return self.from_expr(Kind.BV_XOR, self._term, other._term)
+
 
 S = TypeVar("S", bound=Symbolic)
 
 
 class Constraint(Symbolic):
-    def __init__(self, value: bool | str):
+    def __init__(self, value: bool | str, /):
         if isinstance(value, str):
             term = ctx.bzla.mk_const(ctx.bool_sort, value)
         elif isinstance(value, bool):  # pyright: ignore[reportUnnecessaryIsInstance]
@@ -48,10 +74,7 @@ class Constraint(Symbolic):
             assert_never(value)
         super().__init__(term)
 
-    def __invert__(self) -> Constraint:
-        return self.from_expr(Kind.NOT, self._term)
-
-    def ite(self, then: S, else_: S) -> S:
+    def ite(self, then: S, else_: S, /) -> S:
         return then.from_expr(Kind.ITE, self._term, then._term, else_._term)
 
 
@@ -61,7 +84,7 @@ N = TypeVar("N", bound=int)
 class BitVector(Symbolic, Generic[N]):
     _width: Final[int] = 0
 
-    def __class_getitem__(cls, key: TypeVar) -> type[Self]:
+    def __class_getitem__(cls, key: TypeVar, /) -> type[Self]:
         args = get_args(key)
         if len(args) == 0:
             return cls
@@ -82,7 +105,7 @@ class BitVector(Symbolic, Generic[N]):
         else:
             raise TypeError(f"Unknown type parameter passed to {cls.__name__}[...].")
 
-    def __init__(self, value: int | str) -> None:
+    def __init__(self, value: int | str, /) -> None:
         if self._width == 0:
             raise TypeError(
                 f"Cannot instantiate {self.__class__.__name__} directly; "
@@ -97,74 +120,68 @@ class BitVector(Symbolic, Generic[N]):
             assert_never(value)
         super().__init__(term)
 
-    def __eq__(  # pyright: ignore[reportIncompatibleMethodOverride]
-        self, other: Self
-    ) -> Constraint:
-        """
-        >>> Uint8(1) == Uint8(3)
-        Constraint(`false`)
-        """
-        return Constraint.from_expr(Kind.EQUAL, self._term, other._term)
+    @abc.abstractmethod
+    def __lt__(self, other: Self, /) -> Constraint:
+        ...
 
-    def __ne__(  # pyright: ignore[reportIncompatibleMethodOverride]
-        self, other: Self
-    ) -> Constraint:
-        return Constraint.from_expr(Kind.DISTINCT, self._term, other._term)
+    @abc.abstractmethod
+    def __le__(self, other: Self, /) -> Constraint:
+        ...
 
-    def __add__(self, other: Self) -> Self:
+    def __add__(self, other: Self, /) -> Self:
         return self.from_expr(Kind.BV_ADD, self._term, other._term)
 
-    def __sub__(self, other: Self) -> Self:
+    def __sub__(self, other: Self, /) -> Self:
         return self.from_expr(Kind.BV_SUB, self._term, other._term)
 
-    def __mul__(self, other: Self) -> Self:
+    def __mul__(self, other: Self, /) -> Self:
         return self.from_expr(Kind.BV_MUL, self._term, other._term)
 
-    def __lshift__(self: Self, other: Self) -> Self:
+    @abc.abstractmethod
+    def __floordiv__(self, other: Self, /) -> Self:
+        ...
+
+    @abc.abstractmethod
+    def __mod__(self, other: Self, /) -> Self:
+        ...
+
+    def __lshift__(self, other: Uint[N], /) -> Self:
         return self.from_expr(Kind.BV_SHL, self._term, other._term)
+
+    @abc.abstractmethod
+    def __rshift__(self, other: Uint[N], /) -> Self:
+        ...
 
 
 class Uint(BitVector[N]):
-    def __lt__(self: Self, other: Self) -> Constraint:
+    def __lt__(self, other: Self, /) -> Constraint:
         return Constraint.from_expr(Kind.BV_ULT, self._term, other._term)
 
-    def __le__(self: Self, other: Self) -> Constraint:
+    def __le__(self, other: Self, /) -> Constraint:
         return Constraint.from_expr(Kind.BV_ULE, self._term, other._term)
 
-    def __floordiv__(self: Self, other: Self) -> Self:
+    def __floordiv__(self, other: Self, /) -> Self:
         return self.from_expr(Kind.BV_UDIV, self._term, other._term)
 
-    def __mod__(self: Self, other: Self) -> Self:
+    def __mod__(self, other: Self, /) -> Self:
         return self.from_expr(Kind.BV_UREM, self._term, other._term)
 
-    def __rshift__(self: Self, other: Self) -> Self:
+    def __rshift__(self, other: Uint[N], /) -> Self:
         return self.from_expr(Kind.BV_SHR, self._term, other._term)
-
-    def __and__(self: Self, other: Self) -> Self:
-        return self.from_expr(Kind.BV_AND, self._term, other._term)
-
-    def __or__(self: Self, other: Self) -> Self:
-        return self.from_expr(Kind.BV_OR, self._term, other._term)
-
-    def __xor__(self: Self, other: Self) -> Self:
-        return self.from_expr(Kind.BV_XOR, self._term, other._term)
-
-    def __invert__(self: Self) -> Self:
-        return self.from_expr(Kind.BV_NOT, self._term)
 
 
 class Int(BitVector[N]):
-    def __lt__(self: Self, other: Self) -> Constraint:
+    def __lt__(self, other: Self, /) -> Constraint:
         return Constraint.from_expr(Kind.BV_SLT, self._term, other._term)
 
-    def __le__(self: Self, other: Self) -> Constraint:
+    def __le__(self, other: Self, /) -> Constraint:
         return Constraint.from_expr(Kind.BV_SLE, self._term, other._term)
 
-    def __floordiv__(self: Self, other: Self) -> Self:
+    def __floordiv__(self, other: Self, /) -> Self:
         return self.from_expr(Kind.BV_SDIV, self._term, other._term)
 
-    def __mod__(self: Self, other: Self) -> Self:
+    def __mod__(self, other: Self, /) -> Self:
         return self.from_expr(Kind.BV_SREM, self._term, other._term)
 
-    def __rshift__(self: Self, other: Self) -> Self:
+    def __rshift__(self, other: Uint[N], /) -> Self:
         return self.from_expr(Kind.BV_ASHR, self._term, other._term)
